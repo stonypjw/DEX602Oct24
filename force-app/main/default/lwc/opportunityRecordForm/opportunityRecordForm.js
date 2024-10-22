c0dfa169b7e72570cadfbe1921247967b2e8e6d9
stonypjw <jeremy.white@stonyp.com>
2024-10-22 10:22:33 -0700

    new file:   force-app/main/default/lwc/opportunityRecordForm/__tests__/opportunityRecordForm.test.js 	new file:   force-app/main/default/lwc/opportunityRecordForm/opportunityRecordForm.html 	new file:   force-app/main/default/lwc/opportunityRecordForm/opportunityRecordForm.js 	new file:   force-app/main/default/lwc/opportunityRecordForm/opportunityRecordForm.js-meta.xml

---
+++ force-app/main/default/lwc/opportunityRecordForm/opportunityRecordForm.js
@@ -0,0 +1,9 @@
+import { LightningElement,api } from 'lwc';
+
+export default class OpportunityRecordForm extends LightningElement {
+
+    @api recordId; //autopopulated
+    @api objectApiName = 'Opportunity';
+    @api layoutType = 'Compact';
+    @api formMode = "readonly";
+}
