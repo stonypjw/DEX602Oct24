e00eeb058647dbed1dfbdf3dbc26f2d2ba47ed60
stonypjw <jeremy.white@stonyp.com>
2024-10-22 14:24:21 -0700

    modified:   force-app/main/default/lwc/opportunityList/opportunityList.html 	modified:   force-app/main/default/lwc/opportunityList/opportunityList.js

--- force-app/main/default/lwc/opportunityList/opportunityList.js
+++ force-app/main/default/lwc/opportunityList/opportunityList.js
@@ -7,8 +7,11 @@ export default class OpportunityList extends LightningElement {
 
      recordsToDisplay = false;
      displayedOpps = [];
+     allOpps = [];
      results;
      status = 'All';
+     totalRecords;
+     totalAmount;
 
      @track comboOptions = [
         {value: 'All', label: 'All' },
@@ -17,18 +20,64 @@ export default class OpportunityList extends LightningElement {
         {value: 'ClosedWon', label: 'All Won' },
         {value: 'ClosedLost', label: 'All Lost' },
      ];
-     
+
      @wire(getOpportunities, { accountId: '$recordId'})
      wiredOpps(oppRecords){
         this.results = oppRecords;
 
         if(this.results.data){
-            this.displayedOpps = this.results.data;
-            this.recordsToDisplay = true;
+            this.allOpps = this.results.data;
+            this.updateList();
         }
         if(this.results.error){
             this.recordsToDisplay = false;
             console.error('Error occured retrieving opp records');
         }
+     }
+
+     updateList(){
+        this.displayedOpps = [];
+
+        let currentRecord = {};
+
+        if (this.status === 'All'){
+            this.displayedOpps = this.allOpps;
+        }
+        else {
+            for (let i=0; i < this.allOpps.length; i++){
+                currentRecord = this.allOpps[i];
+                if(this.status === 'Open'){
+                    if(!currentRecord.IsClosed){
+                        this.displayedOpps.push(currentRecord);
+                    }
+                }
+                else if (this.status === 'Closed'){
+                    if(currentRecord.IsClosed){
+                        this.displayedOpps.push(currentRecord);
+                    }
+                }
+                else if(this.status === 'ClosedWon'){
+                    if(currentRecord.IsWon){
+                        this.displayedOpps.push(currentRecord);
+                    } 
+                }
+                else if(this.status === 'ClosedLost'){
+                    if(!currentRecord.IsWon && currentRecord.IsClosed){
+                        this.displayedOpps.push(currentRecord);
+                    } 
+                }
+            }
+        }
+        this.recordsToDisplay = this.displayedOpps.length > 0 ? true : false;
+
+        this.totalRecords = this.displayedOpps.length;
+        this.totalAmount = this.displayedOpps.reduce((prev,curr) => prev + (isNaN(curr.Amount) ? 0 : curr.Amount), 0);
+
+
+     }
+
+     handleChange(event) {
+        this.status = event.detail.value;
+        this.updateList();
      }
 }
