97ee63480e38bd1fa6c6757a3f877f2f13138282
stonypjw <jeremy.white@stonyp.com>
2024-10-22 13:43:14 -0700

    modified:   force-app/main/default/lwc/opportunityList/opportunityList.html 	modified:   force-app/main/default/lwc/opportunityList/opportunityList.js

--- force-app/main/default/lwc/opportunityList/opportunityList.js
+++ force-app/main/default/lwc/opportunityList/opportunityList.js
@@ -1,4 +1,4 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import getOpportunities from '@salesforce/apex/OpportunityController.getOpportunities';
 
 export default class OpportunityList extends LightningElement {
@@ -8,7 +8,16 @@ export default class OpportunityList extends LightningElement {
      recordsToDisplay = false;
      displayedOpps = [];
      results;
+     status = 'All';
 
+     @track comboOptions = [
+        {value: 'All', label: 'All' },
+        {value: 'Open', label: 'All Open' },
+        {value: 'Closed', label: 'All Closed' },
+        {value: 'ClosedWon', label: 'All Won' },
+        {value: 'ClosedLost', label: 'All Lost' },
+     ];
+     
      @wire(getOpportunities, { accountId: '$recordId'})
      wiredOpps(oppRecords){
         this.results = oppRecords;
