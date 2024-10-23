cf3fa546f2ccca10c45d74444b60444d32eb1912
stonypjw <jeremy.white@stonyp.com>
2024-10-23 07:29:21 -0700

    modified:   force-app/main/default/lwc/oppCard/oppCard.html  modified:   force-app/main/default/lwc/oppCard/oppCard.js 	modified:   force-app/main/default/lwc/opportunityList/opportunityList.html

--- force-app/main/default/lwc/oppCard/oppCard.js
+++ force-app/main/default/lwc/oppCard/oppCard.js
@@ -1,8 +1,20 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class OppCard extends LightningElement {
+export default class OppCard extends NavigationMixin(LightningElement) {
     @api name;
     @api stage;
     @api amount;
     @api closeDate;
+    @api oppId;
+
+    viewRecord(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.oppId,
+                actionName: 'view' 
+            }
+        });
+    }
 }
